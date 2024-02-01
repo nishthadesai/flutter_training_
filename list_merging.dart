ace06eefc1f77db7ba5225a74623c57100578230
Nishtha Desai <nishthadesai1611@gmail.com>
2024-02-01 19:21:14 +0530

    list_merging

---
+++ list_merging.dart
@@ -0,0 +1,15 @@
+// merge two lists of integers into a single list.
+import 'dart:io';
+
+// void merging() {
+//   print("enter first list");
+//   List<String> firstList = stdin.readLineSync().toString().split(' ');
+//   print("enter second list");
+//   List<String> secondList = stdin.readLineSync().toString().split(' ');
+//   firstList.addAll(secondList);
+//   print(firstList);
+// }
+void detail()
+{
+  Map map={};
+}
